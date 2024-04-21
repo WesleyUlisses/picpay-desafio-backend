df28fb4fe981db7e029eef501f7670f1d3775856
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-04-21 19:34:47 -0300

    Add BusinessUser constructor with parameters in BusinessUser.cs

--- DesafioPicPay/CarteiraDigital/Domain/Entities/BusinessUser.cs
+++ DesafioPicPay/CarteiraDigital/Domain/Entities/BusinessUser.cs
@@ -2,6 +2,28 @@
 
 public class BusinessUser : User
 {
+    public BusinessUser(string name, string email, string password, string phoneNumber, Address address, string cnpj, string socialReason, string fantasyName, string stateRegistration, string municipalRegistration, string activity, int numberOfEmployees, string legalNature, double annualBilling)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        PhoneNumber = phoneNumber;
+        Address = address;
+        CNPJ = cnpj;
+        SocialReason = socialReason;
+        FantasyName = fantasyName;
+        StateRegistration = stateRegistration;
+        MunicipalRegistration = municipalRegistration;
+        Activity = activity;
+        NumberOfEmployees = numberOfEmployees;
+        LegalNature = legalNature;
+        AnnualBilling = annualBilling;
+    }
+
+    public BusinessUser()
+    {
+    }
+    
     public string CNPJ { get; set; }
     public string SocialReason { get; set; }
     public string FantasyName { get; set; }
